f9b61f5d1f14655ad2bf9034e4e8637007d9dcb8
franklwambo <fncopiyo@gmail.com>
2019-05-14 20:17:37 +0300

    Automate the execution of an R script

---
+++ Automate_R_Script.bat
@@ -0,0 +1,8 @@
+REM to run an R script as a windows process
+
+REM navigate to C:\Program Files\R\R-3.5.2\bin (or your R version)
+cd "C:\Program Files\R\R-3.5.2\bin"
+
+REM run the file using Rscript "Path-to-RFile-to-Execute.R" 
+
+Rscript ANC_Data_Queries_Round2.R
